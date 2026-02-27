994a012b23027f4eeb5d27d13ad5192926ce1e65
Julian <jkf.siemens@gmail.com>
2026-02-27 17:59:19 +0000

    Template added to prevent crashes
    Claude-recommended template to prevent states from crashing on call.

---
+++ settings.py
@@ -0,0 +1,18 @@
+import pygame
+
+#screen:
+info = pygame.display.Info()
+WIDTH, HEIGHT  = info.current_w, info.current_h
+FPS = 60
+TITLE = "Atlas Rising"
+#colours:
+
+#game rules:
+
+#fonts
+
+#rocket
+
+#animation timers
+
+#gas laws questions
---
+++ states/game_over.py
@@ -0,0 +1,20 @@
+import pygame
+from settings import WIDTH, HEIGHT, WHITE, BLACK
+
+class game_over:
+    def __init__(self, game):
+        self.game = game
+
+    #initialise everything your state needs
+
+def handle_event(self, event):
+    #clicks and user inputs go here
+    pass
+
+def update(self, dt):
+    #state logic to go here
+    pass
+
+def draw(self,screen):
+    #all animations here.
+    pass
